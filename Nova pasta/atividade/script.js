42c18665b0b3300297b4207b9f3ea324044cd77a
renan1601 <162443998+renan1601@users.noreply.github.com>
2024-06-12 08:46:10 -0300

    Add files via upload

---
+++ Nova pasta/atividade/script.js
@@ -0,0 +1,32 @@
+console.log("Bem-vindo!");
+
+//Soma
+let n1 = 5;
+let n2 = 10;
+let soma = n1 + n2;
+console.log("A soma é: " + soma);
+
+//Subtração
+let n3 = 10;
+let n4 = 5;
+let subtracao = n3 - n4;
+console.log("A subtração é: " + subtracao);
+
+//Multiplicação 
+let n5 = 5;
+let n6 = 10;
+let multiplicacao = n5 * n6;
+console.log("A multiplicação é: " + multiplicacao);
+
+//Divisão
+let n7 = 10;
+let n8 = 2;
+let divisao = n7 / n8;
+console.log("A divisão é: " + divisao);
+
+//Cálculo 
+let n9 = 5;
+let n10 = 10;
+let n11 = 15;
+let media = (n9 + n10 + n11) / 3;
+console.log("A média é: " + media);
